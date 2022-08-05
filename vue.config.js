6dfa4fe264ec78fa536e8652390d7fc8ec0fac13
Stephan Strittmatter <stephan@st-strittmatter.name>
2022-08-05 13:05:07 +0200

    add pwa config

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.export = {
+    pwa: {
+        name: 'SMITER',
+        themeColor: '#323232',
+        msTileColor: '#323232',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black-translucent'
+    }
+}
