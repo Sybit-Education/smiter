1266ad70dc5e236cd5695b1e97af4d7bcd759e32
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-02-12 14:21:33 +0100

    Dev (#110)
    * Add qr-code generator to smiter (WIP)
    
    * chore: add year
    
    * fix: hide origin image
    
    * chore: bump versions
    
    * chore: layout refactoring
    
    * chore: to vite migrated
    
    * chore: updated configs
    
    * chore: optimize layout
    
    * chore: reorg libs
    
    * reorg stylings
    
    * fix: base url env
    
    ---------
    
    Co-authored-by: rbu <rbu@sybit.de>

--- src/components/ManipulationControl.vue
+++ src/components/ManipulationControl.vue
@@ -3,108 +3,113 @@
       <tr>
         <td class="north-west"> </td>
         <td class="north">
-          <a-button @click="moveUp" class="moveUp">▲</a-button></td>
+          <a-button @click="moveUp" class="moveUp" ><ArrowUpOutlined /></a-button></td>
         <td class="north-east"> </td>
       </tr>
       <tr>
         <td class="west">
-          <a-button @click="moveLeft" class="moveLeft">◀</a-button>
+          <a-button @click="moveLeft" class="moveLeft" ><ArrowLeftOutlined /></a-button>
         </td>
-        <td class="center">
-          <a-button @click="zoomOut" class="zoomOut">-</a-button>
-          <a-button @click="zoomIn" class="zoomIn">+</a-button>
+        <td class="center-zoom">
+          <a-button @click="zoomOut" class="zoomOut"><MinusOutlined /></a-button>
+          <a-button @click="zoomIn" class="zoomIn"><PlusOutlined /></a-button>
         </td>
         <td class="east">
-          <a-button @click="moveRight" class="moveRight">▶</a-button>
+          <a-button @click="moveRight" class="moveRight"><ArrowRightOutlined /></a-button>
         </td>
       </tr>
       <tr>
         <td class="south-west"></td>
         <td class="south">
-          <a-button @click="moveDown" class="moveDown">▼</a-button></td>
+          <a-button @click="moveDown" class="moveDown"><ArrowDownOutlined /></a-button>
+        </td>
         <td class="south-east">  </td>
       </tr>
     </table>
 </template>
 
 <script>
+import { ArrowDownOutlined, ArrowLeftOutlined, ArrowRightOutlined, ArrowUpOutlined, MinusOutlined, PlusOutlined } from '@ant-design/icons-vue'
+
 export default {
-  name: 'ManipulationControl',
-  methods: {
-    moveUp () {
-      this.$emit('moveUp')
-    },
-    moveDown () {
-      this.$emit('moveDown')
-    },
-    moveLeft () {
-      this.$emit('moveLeft')
-    },
-    moveRight () {
-      this.$emit('moveRight')
-    },
-    zoomIn () {
-      this.$emit('zoomIn')
-    },
-    zoomOut () {
-      this.$emit('zoomOut')
-    },
-  }
+    name: 'ManipulationControl',
+    components: { ArrowUpOutlined, ArrowDownOutlined, ArrowLeftOutlined, ArrowRightOutlined, PlusOutlined, MinusOutlined },
+    methods: {
+        moveUp() {
+            this.$emit('moveUp');
+        },
+        moveDown() {
+            this.$emit('moveDown');
+        },
+        moveLeft() {
+            this.$emit('moveLeft');
+        },
+        moveRight() {
+            this.$emit('moveRight');
+        },
+        zoomIn() {
+            this.$emit('zoomIn');
+        },
+        zoomOut() {
+            this.$emit('zoomOut');
+        },
+    }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 .manipulation-control {
   margin: 0.25rem;
 
   .north-west {
       width: 3rem;
-      border-top-left-radius: 6rem;
+      height: 3rem;
+      border-top-left-radius: 100%;
       background-color: grey;
   }
   .north {
-      width: 3rem;
-      height: 3rem;
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
   }
   .north-east {
-      width: 3rem;
-      height: 3rem;
-      border-top-right-radius: 6rem;
+      width: 4.5rem;
+      height: 4.5rem;
+      border-top-right-radius: 100%;
       background-color: grey;
   }
   .west {
-      width: 3rem;
-      height: 3rem;
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
   }
-  .center {
-      width: 3rem;
-      height: 3rem;
+  .center-zoom {
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
   }
   .east {
-      width: 3rem;
-      height: 3rem;
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
   }
   .south-west {
-      width: 3rem;
-      height: 3rem;
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
-      border-bottom-left-radius: 6rem;
+      border-bottom-left-radius: 100%;
   }
   .south {
-      width: 3rem;
-      height: 3rem;
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
   }
   .south-east {
-      width: 3rem;
-      height: 3rem;
+      width: 4.5rem;
+      height: 4.5rem;
       background-color: grey;
-      border-bottom-right-radius: 9rem;
+      border-bottom-right-radius: 100%;
   }
   .ant-btn {
     border-style: none;
@@ -112,18 +117,22 @@ export default {
     font-weight: 900;
     width: 4.5rem;
     height: 4.5rem;
+    border-radius: 0;
+    border: 0;
   }
   .zoomOut {
     width: 4.5rem;
     height: 2.25rem;
-    border-top-left-radius: 2rem;
-    border-top-right-radius: 2rem;
+    padding: 0;
+    border-top-left-radius: 2.25rem;
+    border-top-right-radius: 2.25rem;
   }
   .zoomIn {
     width: 4.5rem;
     height: 2.25rem;
-    border-bottom-left-radius: 2rem;
-    border-bottom-right-radius: 2rem;
+    padding: 0;
+    border-bottom-left-radius: 2.25rem;
+    border-bottom-right-radius: 2.25rem;
   }
 }
 </style>
--- src/service/airtable.service.js
+++ src/service/airtable.service.js
@@ -1,7 +1,7 @@
 import Airtable from 'airtable'
 
 export const api = new Airtable({
-  apiKey: `${process.env.VUE_APP_AIRTABLE_API_KEY}`
-}).base(`${process.env.VUE_APP_AIRTABLE_BASE}`)
+  apiKey: `${import.meta.env.VITE_APP_AIRTABLE_API_KEY}`
+}).base(`${import.meta.env.VITE_APP_AIRTABLE_BASE}`)
 
 export default api
