9bab4ffb286f3777b50ed50aa34cc794bfbd993c
Stephan Strittmatter <stritti@users.noreply.github.com>
2024-02-21 11:17:23 +0100

    chore: refactored to About-Tab

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,40 @@
 <template>
   <a-layout>
+    <a-config-provider :theme="{ token: { colorPrimary: '#b51783', borderRadius: '8px' } }">
 
     <a-layout-header>
-      <h1>SMITER - Social Media Image Templater</h1>
+      SMITER - Social Media Image Templater
     </a-layout-header>
 
     <a-layout-content>
-      <a-tabs id="tabs" v-model:activeKey="activeKey" size="large" tab-position="bottom"
-              type="card"
-              :tabBarStyle="{margin: '0 0 0 0'}"
-      >
-        <a-tab-pane id="tab" key="1" tab="Templates">
+      <a-tabs id="tabs" v-model:activeKey="activeKey" size="large" tab-position="bottom"  type="card">
+        <a-tab-pane key="1" class="content" tab="Templates" force-render>
           <image-template />
         </a-tab-pane>
-        <a-tab-pane key="2" tab="vCard" force-render>
+        <a-tab-pane key="2" class="content" tab="vCard" force-render>
           <QRCode/>
+        </a-tab-pane>
+        <a-tab-pane key="3" class="content" tab="About">
+          <about-component />
         </a-tab-pane>
       </a-tabs>
 
     </a-layout-content>
-
-    <a-layout-footer class="background">
-      <footer-bar />
-    </a-layout-footer>
+    </a-config-provider>
   </a-layout>
 
 </template>
 
 <script>
 import {ref} from 'vue'
-import FooterBar from '@/components/FooterBar.vue'
+import AboutComponent from '@/views/AboutComponent.vue'
 import QRCode from '@/views/QRCode.vue'
 import ImageTemplate from '@/views/ImageTemplate.vue'
 
 export default {
   name: 'App',
   components: {
-    FooterBar, QRCode, ImageTemplate
+    AboutComponent, QRCode, ImageTemplate
   },
   setup() {
     return {
@@ -49,13 +47,18 @@ export default {
 <style scoped>
 
 @import '@/assets/css/custom.css';
-h1 {
+.ant-layout-header {
+  background-color: #b51783;
   color: whitesmoke;
+  position: 'fixed';
+  z-index: 1;
+  width: '100%';
+  font-size: 1.25rem;
 }
 
-.ant-layout-content {
-  min-height: calc(100vh - 9rem);
+.content {
+  min-height: calc(100vh - 120px);
+  max-height: calc(100vh - 120px);
+  overflow: scroll;
 }
-
-
 </style>
---
+++ src/views/AboutComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <a-space direction="vertical">
+      <div>Developed with 💜 for you!</div>
+      <div>&copy; {{ year }} Sybit GmbH</div>
+      <div><a href="https://www.sybit.de/impressum">Impressum</a> </div>
+      <div><a href="https://www.sybit.de/datenschutz">Datenschutz</a></div>
+    </a-space>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar',
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    }
+  }
+}
+</script>
+
+<style scoped>
+.about {
+  margin: 0.5rem;
+}
+</style>
