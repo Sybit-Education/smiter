1df17479a3fd4e2ffb3d7d70d64f9183608bc36a
Strittmatter, Stephan <stephan.strittmatter@sybit.de>
2024-03-25 11:07:21 +0100

    chore: improoved layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <a-layout>
-    <a-config-provider :theme="{ token: { colorPrimary: '#b51783', borderRadius: '8px' } }">
+  <a-app>
+    <a-layout>
+      <a-config-provider :theme="{ token: { colorPrimary: '#b51783', borderRadius: '8px' } }">
 
-    <a-layout-header>
-      SMITER - Social Media Image Templater
-    </a-layout-header>
+        <a-layout-header class="header">
+          <div>SMITER - <small>Social Media Image Templater</small></div>
+        </a-layout-header>
 
-    <a-layout-content>
-      <a-tabs id="tabs" v-model:activeKey="activeKey" size="large" tab-position="bottom"  type="card">
-        <a-tab-pane key="1" class="content" tab="Templates" force-render>
-          <image-template />
-        </a-tab-pane>
-        <a-tab-pane key="2" class="content" tab="vCard" force-render>
-          <QRCode/>
-        </a-tab-pane>
-        <a-tab-pane key="3" class="content" tab="About">
-          <about-component />
-        </a-tab-pane>
-      </a-tabs>
+        <a-layout-content>
+          <a-tabs id="tabs" v-model:activeKey="activeKey" size="large" tab-position="bottom" type="card">
+            <a-tab-pane key="1" class="content" tab="Templates" force-render>
+              <image-template />
+            </a-tab-pane>
+            <a-tab-pane key="2" class="content" tab="vCard" force-render>
+              <QRCode/>
+            </a-tab-pane>
+            <a-tab-pane key="3" class="content" tab="About">
+              <about-component />
+            </a-tab-pane>
+          </a-tabs>
 
-    </a-layout-content>
-    </a-config-provider>
-  </a-layout>
-
+        </a-layout-content>
+      </a-config-provider>
+    </a-layout>
+  </a-app>
 </template>
 
 <script>
@@ -52,9 +53,14 @@ export default {
   color: whitesmoke;
   position: 'fixed';
   z-index: 1;
-  width: '100%';
   font-size: 1.25rem;
+  div {
+    width: '100%';
+    height: 1.25rem;
+    text-overflow: 'clip';
+  }
 }
+
 
 .content {
   min-height: calc(100vh - 120px);
